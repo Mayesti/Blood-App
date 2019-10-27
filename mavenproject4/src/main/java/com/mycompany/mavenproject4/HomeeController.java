43ca2643ba31eb70b8fee9136bd369ac0e8ee641
jessiva19 <54264420+jessiva19@users.noreply.github.com>
2019-10-27 21:22:40 +0700

    edit relasi tambah data

--- mavenproject4/src/main/java/com/mycompany/mavenproject4/HomeeController.java
+++ mavenproject4/src/main/java/com/mycompany/mavenproject4/HomeeController.java
@@ -5,6 +5,7 @@
  */
 package com.mycompany.mavenproject4;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -13,8 +14,12 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -27,7 +32,19 @@ public class HomeeController implements Initializable {
 
     @FXML
     private Label label;
-
+    
+    @FXML
+    void btntambah(ActionEvent event) throws IOException, SQLException{
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/tambah.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));  
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
     @FXML
     void btnRiwayat(ActionEvent event) {
 
