e133fe72fb021ecf34b8f8d3efbdc319bd3ec279
cynthia1331 <54264416+cynthia1331@users.noreply.github.com>
2019-10-27 22:02:16 +0700

    fix signup

--- mavenproject4/src/main/java/com/mycompany/mavenproject4/SignUpController.java
+++ mavenproject4/src/main/java/com/mycompany/mavenproject4/SignUpController.java
@@ -6,14 +6,27 @@ package com.mycompany.mavenproject4;
  * and open the template in the editor.
  */
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -43,10 +56,42 @@ public class SignUpController implements Initializable {
     @FXML
     private RadioButton rbWanita;
     
+    private Connection con = null;
     
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private void btnRegister(ActionEvent event) throws SQLException, IOException{
+//        System.out.println("clicked btn login");
+        String username = tfUsername.getText();
+        String email = tfEmail.getText();
+        String tgl = dpTgl.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String gender;
+        if(rbPria.isSelected()){
+            gender = "L";
+        }else{
+            gender = "P";
+        }
+        String password = pfPassword.getText();
+        String konfirm = pfKonfirm.getText();
+        
+            
+            String url = "jdbc:sqlite:logiin.db";
+            con = DriverManager.getConnection(url);
+            Statement p = con.createStatement();
+//            System.out.println(usern);
+//            System.out.println(pass);
+            String insert = "insert into user (username, password, email, tgl_lahir, jenis_kelamin) values ('"+username+"','"+password+"','"+email+"','"+tgl+"','"+gender+"')";
+            int q = p.executeUpdate(insert);
+                
+            if(q == 1){
+                System.out.println("Berhasil");
+            }else{
+                System.out.println("Gagal");
+            }
+            
+                
+        } 
+    
+     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
