c5ea732aa10dd8ca32109a11ee63926b2502de69
jessiva19 <54264420+jessiva19@users.noreply.github.com>
2019-11-01 10:06:18 +0700

    edit homee -> menu

---
+++ mavenproject4/src/main/java/com/mycompany/mavenproject4/MenuController.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject4;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author ASUS
+ */
+public class MenuController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    private void btntambahdata(ActionEvent event) throws IOException, SQLException{
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/menu.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));  
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void btnriwayat(ActionEvent event) throws IOException, SQLException{
+
+    }
+
+    @FXML
+    private void btnanalisis(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void btngrafik(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
