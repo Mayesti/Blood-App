0164a273308a3bedb833871536fa6f9a00f12066
cynthia1331 <54264416+cynthia1331@users.noreply.github.com>
2019-11-28 20:01:44 +0700

    fix grafik

--- mavenproject4/src/main/java/com/mycompany/mavenproject4/GrafikController.java
+++ mavenproject4/src/main/java/com/mycompany/mavenproject4/GrafikController.java
@@ -18,7 +18,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
@@ -38,6 +40,12 @@ public class GrafikController implements Initializable {
     private LineChart chart;
     
     @FXML
+    private CategoryAxis xAxis;
+    
+    @FXML
+    private NumberAxis yAxis;
+    
+    @FXML
     void btnBack(ActionEvent event) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/menu.fxml"));
@@ -55,12 +63,15 @@ public class GrafikController implements Initializable {
      */
     @FXML
     private void cbJenisOnAction(){
+        
         if(cbJenis.getSelectionModel().getSelectedIndex()==0){
+            yAxis.setLabel("mmHg");
             XYChart.Series<String, Integer> sistolSeries = new XYChart.Series<>(); 
             XYChart.Series<String, Integer> diastolSeries = new XYChart.Series<>();
+//            XYChart.Series<String, Integer> pulseSeries = new XYChart.Series<>();
             sistolSeries.setName("Sistolik");
             diastolSeries.setName("Diastolik");
-            
+//            pulseSeries.setName("Pulse");            
             try{
                 String sql = "SELECT * FROM data WHERE username = '"+UserLogin.username+"'";
                 Connection con = Db.connectDB();
@@ -70,8 +81,10 @@ public class GrafikController implements Initializable {
                     String tanggal = rs.getString("waktu_tambah");
                     int sistol = rs.getInt("sistol");
                     int diastol = rs.getInt("diastol");
+//                    int pulse = rs.getInt("pulse");
                     sistolSeries.getData().add(new XYChart.Data<>(tanggal, sistol));
                     diastolSeries.getData().add(new XYChart.Data<>(tanggal, diastol));
+//                    pulseSeries.getData().add(new XYChart.Data<>(tanggal, pulse));
                 }
                 con.close();
             }catch(SQLException e){
@@ -80,8 +93,50 @@ public class GrafikController implements Initializable {
             chart.getData().clear();
             chart.getData().add(sistolSeries);
             chart.getData().add(diastolSeries);
-        }else if(cbJenis.getSelectionModel().getSelectedIndex()==1){
+//            chart.getData().add(pulseSeries);
             
+        }else if(cbJenis.getSelectionModel().getSelectedIndex()==1){
+            yAxis.setLabel("mg/dL");
+            XYChart.Series<String, Integer> gulaSeries = new XYChart.Series<>(); 
+            gulaSeries.setName("Kadar Gula");
+            
+            try{
+                String sql = "SELECT * FROM data WHERE username = '"+UserLogin.username+"'";
+                Connection con = Db.connectDB();
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+                while(rs.next()){
+                    String tanggal = rs.getString("waktu_tambah");
+                    int gula = rs.getInt("gula_darah");
+                    gulaSeries.getData().add(new XYChart.Data<>(tanggal, gula));
+                }
+                con.close();
+            }catch(SQLException e){
+                showMessageDialog(null, e.getMessage());
+            }
+            chart.getData().clear();
+            chart.getData().add(gulaSeries);
+        }else if(cbJenis.getSelectionModel().getSelectedIndex()==2){
+            yAxis.setLabel("bpm");
+            XYChart.Series<String, Integer> pulseSeries = new XYChart.Series<>(); 
+            pulseSeries.setName("Denyut Nadi (Pulse)");
+            
+            try{
+                String sql = "SELECT * FROM data WHERE username = '"+UserLogin.username+"'";
+                Connection con = Db.connectDB();
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+                while(rs.next()){
+                    String tanggal = rs.getString("waktu_tambah");
+                    int pulse = rs.getInt("pulse");
+                    pulseSeries.getData().add(new XYChart.Data<>(tanggal, pulse));
+                }
+                con.close();
+            }catch(SQLException e){
+                showMessageDialog(null, e.getMessage());
+            }
+            chart.getData().clear();
+            chart.getData().add(pulseSeries);
         }
     }
     
@@ -90,6 +145,7 @@ public class GrafikController implements Initializable {
         cbJenis.getItems().clear();
         cbJenis.getItems().add("Tekanan Darah");
         cbJenis.getItems().add("Gula Darah");
+        cbJenis.getItems().add("Denyut Nadi");
         cbJenis.getSelectionModel().select(0);
         cbJenisOnAction();
     }    
